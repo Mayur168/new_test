36a0ea327123dc97a0f43483747eeaa5f5a441de
mayur519 <mayurgaikwad7447@gmail.com>
2025-01-03 21:59:11 +0530

    apidone

--- src/components/CameraCapture.js
+++ src/components/CameraCapture.js
@@ -1,9 +1,77 @@
+// import React, { useState, useRef } from "react";
+// import Webcam from "react-webcam";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faCamera, faCameraRotate } from "@fortawesome/free-solid-svg-icons"; // Import icons
+
+// const CameraCapture = ({ closeCamera }) => {
+//   const [image, setImage] = useState(null);
+//   const [facingMode, setFacingMode] = useState("user");
+//   const webcamRef = useRef(null);
+
+//   const switchCamera = () => {
+//     setFacingMode((prevMode) =>
+//       prevMode === "user" ? "environment" : "user"
+//     );
+//   };
+
+//   const captureImage = () => {
+//     if (webcamRef.current) {
+//       const imageSrc = webcamRef.current.getScreenshot();
+//       setImage(imageSrc);
+//       localStorage.setItem("capturedPhoto", imageSrc);
+//       closeCamera();
+//     }
+//   };
+
+//   return (
+//     <div className="camera-container">
+//       <h1>Capture Prescription Photo</h1>
+
+//       {!image && (
+//         <div className="webcam-wrapper">
+//           <Webcam
+//             audio={false}
+//             ref={webcamRef}
+//             screenshotFormat="image/jpeg"
+//             width="100%"
+//             facingMode={facingMode}
+//           />
+
+//           {/* Camera switch button */}
+//           <div className="camera-switch-overlay" onClick={switchCamera}>
+//             <FontAwesomeIcon icon={faCameraRotate} size="2x" />
+//           </div>
+//         </div>
+//       )}
+
+//       {image && (
+//         <div className="image-preview">
+//           <h2>Captured Photo:</h2>
+//           <img src={image} alt="Captured" style={{ width: "100%" }} />
+//         </div>
+//       )}
+
+//       {!image && (
+//         <div className="capture-button-container">
+//           <button className="capture-button" onClick={captureImage}>
+//             <FontAwesomeIcon icon={faCamera} style={{ marginRight: "5px" }} />
+//             Capture Photo
+//           </button>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default CameraCapture;
 import React, { useState, useRef } from "react";
 import Webcam from "react-webcam";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCamera, faCameraRotate } from "@fortawesome/free-solid-svg-icons"; // Import icons
+import { faCamera, faCameraRotate } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import { handleError } from "../utils"; // Import handle Error for displaying errors
 
-const CameraCapture = ({ closeCamera }) => {
+const CameraCapture = ({ closeCamera, onDataReceived }) => {
   const [image, setImage] = useState(null);
   const [facingMode, setFacingMode] = useState("user");
   const webcamRef = useRef(null);
@@ -14,12 +82,61 @@ const CameraCapture = ({ closeCamera }) => {
     );
   };
 
+  const handleApiCall = async (imageUrl) => {
+    try {
+      const apiUrl =
+        "https://django-imageprocessing-api.vercel.app/api/imageprocess/getdata/";
+
+      const response = await axios.get(apiUrl, {
+        params: { image_url: imageUrl },
+      });
+
+      if (response.status === 200) {
+        // Notify the parent component with the data
+          console.log(response.data)
+        onDataReceived(response.data);
+        closeCamera();
+      } else {
+        handleError("Data fetching failed.");
+      }
+    } catch (error) {
+      handleError("Data fetching failed." + error.message);
+    }
+  };
+
+   const handleImageUpload = async (imageSrc) => {
+    try {
+        // Upload image using the hotel api
+        const apiUrl = 'https://hoteltest-six.vercel.app/uploads/upload_image';
+        const formData = new FormData();
+        const base64Image = imageSrc.split(',')[1];
+        const blob = await fetch(`data:image/jpeg;base64,${base64Image}`).then(res => res.blob());
+        formData.append('file', blob, 'captured-image.jpeg');
+        
+        const response = await axios.post(apiUrl, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            },
+        });
+
+        if(response.status === 201) {
+            // Notify the parent component that the image was uploaded
+             handleApiCall(response.data.file_url)
+            
+        } else{
+             handleError("Image upload failed.")
+        }
+    } catch (error) {
+         handleError("Image upload failed. " + error.message)
+    }
+}
+
   const captureImage = () => {
     if (webcamRef.current) {
       const imageSrc = webcamRef.current.getScreenshot();
       setImage(imageSrc);
-      localStorage.setItem("capturedPhoto", imageSrc);
-      closeCamera();
+         handleImageUpload(imageSrc)
+     
     }
   };
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,102 @@
+// import React, { useEffect, useState } from "react";
+// import About from "./About";
+// import Contact from "./Contact";
+// import CameraCapture from "./CameraCapture";
+
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faCamera } from "@fortawesome/free-solid-svg-icons"; // Import camera icon
+
+// const Home = () => {
+//   const [loggedInUser, setLoggedInUser] = useState("");
+//   const [isCameraVisible, setIsCameraVisible] = useState(false);
+
+//   useEffect(() => {
+//     const user = localStorage.getItem("loggedInUser");
+//     setLoggedInUser(user);
+//   }, []);
+
+//   const toggleCamera = () => {
+//     setIsCameraVisible((prev) => !prev);
+//   };
+
+//   return (
+//     <>
+//       {/* Camera Toggle Button (Fixed Position) */}
+//       <div className="camera-button-container">
+//         <button className="camera-toggle" onClick={toggleCamera}>
+//           <FontAwesomeIcon
+//             icon={faCamera}
+//             style={{ fontSize: "1.5rem" }}
+//           />
+//         </button>
+//       </div>
+//       {/* CameraCapture Component */}
+//       {isCameraVisible && (
+//         <CameraCapture
+//           closeCamera={() => setIsCameraVisible(false)} // Close the camera after photo capture
+//         />
+//       )}
+
+//       {/* Hero Section */}
+//       <section className="hero-section">
+//         <div className="hero-content">
+//           <div className="hero-text">
+//             <h1>Welcome to Medilab</h1>
+//             <p>Your trusted medical care provider.</p>
+//           </div>
+
+//           {/* Cards Section */}
+//           <div className="cards-section">
+//             <div className="card"  style={{ backgroundColor: "#1c75c4" }}>
+//               <h2>Why Choose Medilab</h2>
+//               <p>
+//                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+//                 do eiusmod tempor incididunt ut labore et dolore magna
+//                 aliqua. Duis aute irure dolor in reprehenderit asperiores
+//                 dolores sed et. Tenetur quia eos. Autem tempore quibusdam vel
+//                 necessitatibus optio ad corporis.
+//               </p>
+//               <a href="#about" className="learn-more-btn">
+//                 Learn More
+//               </a>
+//             </div>
+//           </div>
+//         </div>
+//       </section>
+
+//       {/* About Section */}
+//       <section
+//         id="about"
+//         style={{ padding: "50px 20px", backgroundColor: "#f8f9fa" }}
+//       >
+//         <About />
+//       </section>
+
+//       {/* Contact Section */}
+//       <section id="contact" style={{ padding: "50px 20px" }}>
+//         <Contact />
+//       </section>
+
+//       {/* Greeting */}
+//       {/* <div style={{ textAlign: 'center', margin: '20px' }}>
+//           {loggedInUser && <h2>Welcome, {loggedInUser}!</h2>}
+//         </div> */}
+//     </>
+//   );
+// };
+
+// export default Home;
 import React, { useEffect, useState } from "react";
 import About from "./About";
 import Contact from "./Contact";
 import CameraCapture from "./CameraCapture";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCamera } from "@fortawesome/free-solid-svg-icons"; // Import camera icon
+import { faCamera } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
   const [loggedInUser, setLoggedInUser] = useState("");
   const [isCameraVisible, setIsCameraVisible] = useState(false);
+  const [apiData, setApiData] = useState(null); // State to hold the API data
 
   useEffect(() => {
     const user = localStorage.getItem("loggedInUser");
@@ -17,6 +105,11 @@ const Home = () => {
 
   const toggleCamera = () => {
     setIsCameraVisible((prev) => !prev);
+  };
+
+  // Callback function to receive data from CameraCapture
+  const handleDataReceived = (data) => {
+      setApiData(data);
   };
 
   return (
@@ -33,10 +126,22 @@ const Home = () => {
       {/* CameraCapture Component */}
       {isCameraVisible && (
         <CameraCapture
-          closeCamera={() => setIsCameraVisible(false)} // Close the camera after photo capture
+          closeCamera={() => setIsCameraVisible(false)}
+          onDataReceived={handleDataReceived}
         />
       )}
 
+      {/* Display data from API */}
+      {apiData && (
+        <div className="api-data-display">
+            <h2>Prescription Photo data:</h2>
+          <pre>{JSON.stringify(apiData, null, 2)}</pre>
+           {apiData.image_url && (
+               <img src={apiData.image_url} alt="Processed"  style={{maxWidth: '500px', maxHeight:'500px', width:'100%', height: 'auto'}}/>
+          )}
+        </div>
+      )}
+        
       {/* Hero Section */}
       <section className="hero-section">
         <div className="hero-content">
@@ -47,7 +152,7 @@ const Home = () => {
 
           {/* Cards Section */}
           <div className="cards-section">
-            <div className="card"  style={{ backgroundColor: "#1c75c4" }}>
+            <div className="card" style={{ backgroundColor: "#1c75c4" }}>
               <h2>Why Choose Medilab</h2>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
@@ -76,11 +181,6 @@ const Home = () => {
       <section id="contact" style={{ padding: "50px 20px" }}>
         <Contact />
       </section>
-
-      {/* Greeting */}
-      {/* <div style={{ textAlign: 'center', margin: '20px' }}>
-          {loggedInUser && <h2>Welcome, {loggedInUser}!</h2>}
-        </div> */}
     </>
   );
 };
